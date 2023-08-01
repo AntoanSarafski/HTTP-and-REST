7f2c007599720425d8406e214fcb7e8b5e22edbe
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-08-01 13:47:28 +0300

    Commit Github-Commit with async and await !

--- Lab/GitHub-Commits/app.js
+++ Lab/GitHub-Commits/app.js
@@ -1,21 +1,29 @@
-function loadCommits() {
+async function loadCommits() {
   const username = document.querySelector("#username").value;
   const repo = document.querySelector("#repo").value;
 
-  if (!username || !repo) {
-    console.log("Error!");
-  }
-
   const list = document.querySelector("ul");
-  list.innerHTML = "";
-  fetch(`https://api.github.com/repos/${username}/${repo}/commits`)
-    .then((res) => res.json())
-    .then((commits) => {
-      commits.forEach((commitObject) => {
-        const item = document.createElement("li");
-        item.textContent = commitObject.commit.message;
 
-        list.appendChild(item);
-      });
-    });
+  const response = await fetch(
+    `https://api.github.com/repos/${username}/${repo}/commits`
+  );
+
+  const body = await response.json();
+
+  body.forEach((commit) => {
+    const item = document.createElement("li");
+    item.textContent = commit.commit.message;
+
+    list.appendChild(item);
+  });
+
+  // .then((res) => res.json())
+  // .then((commits) => {
+  //   commits.forEach((commitObject) => {
+  //     const item = document.createElement("li");
+  //     item.textContent = commitObject.commit.message;
+
+  //     list.appendChild(item);
+  //   });
+  // });
 }
